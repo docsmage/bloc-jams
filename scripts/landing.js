f981308a795188cc750aac27a5c17a8aee1740a7
Renee CS <voiceofrae@gmail.com>
2015-09-13 21:11:05 -0700

    checkpoint 29 lesson complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,32 @@
-var pointsArray = document.getElementsByClassName('point');
- 
- var animatePoints = function(points) {
+var animatePoints = function() {
 	
-	var revealPoint = function () {
-		for (i = 0; i < points.length; i++) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";   		
-		}
+	var revealPoint = function() {
+		$(this).css({
+			opacity: 1,
+			transform: 'scaleX(1) translateY(0)'
+		}); 		
 	};
 	
-	revealPoint();
+ $.each($('.point'), revealPoint);
 
 };
 
- window.onload = function() {
+$(window).load(function() {
  
      // Automatically animates the points on a tall screen where scrolling can't trigger the animation
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+     if ($(window).height() > 950) {
+         animatePoints();
      }
-	 
-     window.addEventListener('scroll', function(event) {
+
+	var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+	
+	$(window).scroll(function(event) {
 			 
-         if (pointsArray[0].getBoundingClientRect().top <= 500) {
-             animatePoints(pointsArray);
-         }
+		if ($(window).scrollTop() >= scrollDistance) {
+			animatePoints();
+		}
      
-		 });
- }
+	});
+
+});
